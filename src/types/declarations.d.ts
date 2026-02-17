78824f44838099b7b7de73519747767197c0d830
vivek <vivekyadav1207vy@gmail.com>
2026-02-17 23:43:58 +0530

    Fix typescript declaration error

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,14 @@
+declare module 'react-syntax-highlighter' {
+  export const Prism: any;
+  export const Light: any;
+}
+
+declare module 'react-syntax-highlighter/dist/esm/styles/prism' {
+  const styles: any;
+  export default styles;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus' {
+    const style: any;
+    export default style;
+}
