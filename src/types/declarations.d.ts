73c3b70465be36659551fecb46e7fda47933b468
vivek <vivekyadav1207vy@gmail.com>
2026-02-18 00:01:54 +0530

    Fix typescript declaration for syntax highlighter path

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,14 +1,3 @@
-declare module 'react-syntax-highlighter' {
-  export const Prism: any;
-  export const Light: any;
-}
-
-declare module 'react-syntax-highlighter/dist/esm/styles/prism' {
-  const styles: any;
-  export default styles;
-}
-
-declare module 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus' {
-    const style: any;
-    export default style;
-}
+declare module 'react-syntax-highlighter';
+declare module 'react-syntax-highlighter/dist/esm/styles/prism';
+declare module 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus';
